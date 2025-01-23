aff25feeaadc3695293f89f8f32c8dae23e103f7
prabaharankathiresan <prabaharan.k0606@gmail.com>
2025-01-23 06:57:13 +0530

    rotate array approach2

--- dsa/arrays/RotateArray.java
+++ dsa/arrays/RotateArray.java
@@ -27,8 +27,26 @@ public class RotateArray {
 
     public static void main(String args[]) {
         int[] nums = {1,2,3,4,5};
-        rotate(nums,2);
+        //rotate(nums,2);
+        approach2(nums,7);
 
+    }
+
+    public static void approach2(int[] nums,int k) {
+        int high = nums.length;
+        for(int i=0;i<k;i++) {            
+            for(int j=high-1;j>0;j--) {                      
+                swap(nums,j,j-1);
+            }
+        }
+        printArray(nums);
+    }
+
+    public static void swap(int[] a, int left, int right) {
+        
+        int temp = a[left];
+        a[left] = a[right];
+        a[right] = temp;
     }
 
 
